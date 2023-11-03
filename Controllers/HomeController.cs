d5204347c0de05701bce60e31c21be59d6deefe7
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-11-03 06:40:06 +0100

    Autenticação implementada com sucesso

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProjectoBaseComIdentity.Models;
 
 namespace ProjectoBaseComIdentity.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
--- Program.cs
+++ Program.cs
@@ -74,7 +74,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.MapRazorPages();
 
