8620ac357c378c9c7e57fa5502b51e8321de8ede
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-11-03 05:25:41 +0100

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectoBaseComIdentity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
