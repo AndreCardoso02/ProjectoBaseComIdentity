8758d7c4020688d9e4d6207e7fda72535776ecc5
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-11-06 04:12:21 +0100

    Actualizar utilizador

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -14,10 +14,11 @@ namespace ProjectoBaseComIdentity.Controllers
     public class AdminController : Controller
     {
         private UserManager<AppUser> _userManager;
-
-        public AdminController(UserManager<AppUser> userManager)
+        private IPasswordHasher<AppUser> _passwordHasher;
+        public AdminController(UserManager<AppUser> userManager, IPasswordHasher<AppUser> passwordHasher)
         {
             _userManager = userManager;
+            _passwordHasher = passwordHasher;
         }
 
         public IActionResult Index()
@@ -25,6 +26,7 @@ namespace ProjectoBaseComIdentity.Controllers
             return View(_userManager.Users);
         }
 
+        // Create
         public ViewResult Create() => View();
 
         [HttpPost]
@@ -51,10 +53,60 @@ namespace ProjectoBaseComIdentity.Controllers
             return View(user);
         }
 
+        // Update
+        public async Task<IActionResult> Update(Guid? id)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id) + "> Id inválido");
+
+            AppUser user = await _userManager.FindByIdAsync(id?.ToString());
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email não pode estar vazio");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Palavra-passe não pode estar vazia");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "Utilizador não foi encontrado");
+            return View(user);
+        }
+
+        #region ErrorsRegion
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
         }
+        #endregion
     }
 }
